0cd357b85af123e59b14bd592a6becb00100264d
Daniel Tsai <zweit@111-241-230-172.dynamic.hinet.net>
2012-04-14 20:47:50 +0800

    6.1 finished

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
+#use "$bundle exec annotate --position before" to annotate the database
+
+class User < ActiveRecord::Base
+  attr_accessible :email, :name
+end
